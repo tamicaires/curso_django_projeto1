ffedf077a4826d595e79db01a0eb616265228234
Thamires Caires <tamiresdec@gmail.com>
2023-08-24 15:13:50 -0300

    Removido contato e sobre

---
+++ recipes/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name':'Thamires',
+        'age':'21'
+    })
